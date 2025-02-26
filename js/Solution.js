93eb8908432f2f87a1b58deb2d589feef1b469a3
denis <denantonov20211@gmail.com>
2025-02-26 18:59:27 +0300

    Solve 6-7 task

--- js/Solution.js
+++ js/Solution.js
@@ -32,3 +32,33 @@ console.log(Resume2);
 /* const lorem = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit harum at excepturi. Praesentium quo, sit vitae autem quis eos est, velit, enim quia placeat soluta consequatur tempore ullam blanditiis incidunt!";
 const lorem2 = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit harum at excepturi. Praesentium quo, sit vitae autem quis eos est, velit, enim quia placeat soluta consequatur tempore ullam blanditiis incidunt!";
 const lorem3 = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit harum at excepturi. Praesentium quo, sit vitae autem quis eos est, velit, enim quia placeat soluta consequatur tempore ullam blanditiis incidunt!"; */
+
+
+//Solve 6 task
+const string = "hello";
+const number = 10;
+const boolean = true; 
+const empty = null;
+const realEmpty = undefined;
+const object = {} ;
+const array = [];
+const symbol = Symbol();
+const bigInt = 10n;
+
+//const resultTypes = `string - ${typeof string}; number - ${typeof number}; boolean - ${typeof boolean}; empty - ${typeof empty}; realEmpty - ${typeof realEmpty}; object - ${typeof object}; array - ${typeof array}; symbol - ${typeof symbol}; bigInt - ${typeof bigInt}`;
+//console.log(resultTypes);
+//Solve 6.1 task
+
+const typesArray = [string,number,boolean,empty,realEmpty,object,array,symbol,bigInt]
+function View(arr){
+    arr.forEach((element,i) => {
+       console.log(`${++i} -${typeof element}`) ;
+    });
+}
+View(typesArray);
+
+//Solve 7 task
+function showName(){
+    console.log("denis");
+}
+showName();
