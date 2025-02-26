d4c42038b8bc77f75fa9eda3e5f374d1b235fd0b
denis <denantonov20211@gmail.com>
2025-02-26 13:32:54 +0300

    Solve 3 task

--- js/Solution.js
+++ js/Solution.js
@@ -1,3 +1,20 @@
 //Solve 1 task
 const myCountry = "Беларусь" ;
 console.log(myCountry);
+
+//Solve 2 task
+let isStudent = '' ; 
+    isStudent = "denis" ;
+    isStudent = "Anton" ;
+if ( isStudent == "denis" ) {
+    console.log('Правда');
+}
+    else {
+        console.log('Ложь')
+    }
+
+//Solve 3 task
+const OneNumber = 10;
+const thoNumber = 15;
+const Result = OneNumber*thoNumber ;
+console.log(Result) ;  
