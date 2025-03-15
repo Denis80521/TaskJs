6c9aafd5b2c090f5bf11041df2bb51252289e1ba
denis <denantonov20211@gmail.com>
2025-03-15 11:23:21 +0300

    TaskSolution12

---
+++ js/task12.js
@@ -0,0 +1,5 @@
+const NBame = "denis"
+const surName = "antonov"
+const favoriteNumber = 5
+const resume = `My name is ${NBame} ${surName} and my favorite number is ${favoriteNumber}`;
+console.log(resume);
